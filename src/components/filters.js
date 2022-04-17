29595a5e026e29b7ec796568b7ceebb3f2030d26
Minh Nguyen <hongminh1620@gmail.com>
2022-04-17 14:45:02 -0700

    Modify the Select

--- src/components/filters.js
+++ src/components/filters.js
@@ -1,88 +1,89 @@
 import React, { useState } from "react";
+import Select from "react-select";
+
 import { DATA_FOR_FILTERS } from "./constant";
 
 export default function Filter() {
   const data = DATA_FOR_FILTERS;
-  const [selectedJobIndustry, setJobIndustry] = useState();
-  const [selectedState, setSelectedState] = useState();
-  const [selectedCity, setSelectedCity] = useState();
-  const [selectedJobType, setJobType] = useState();
+  const [selectedJobIndustry, setJobIndustry] = useState("--Choose Job Industry--");
+  const [selectedState, setSelectedState] = useState("--Choose State--");
+  const [selectedCity, setSelectedCity] = useState("--Choose City--");
+  const [selectedJobType, setJobType] = useState("--Choose Job Type--");
+  const [citiesList, setCitiesList] = useState([]);
+  const jobIndustriesList = [];
+  for (var key in data.jobIndustries) {
+    jobIndustriesList.push(data.jobIndustries[key].name);
+  }
 
-  const availableCities = data.states.find((c) => c.name === selectedState);
+  const statesList = [];
+  for (var key in data.states) {
+    statesList.push(data.states[key].name);
+  }
+
+  const jobTypesList = [];
+  for (var key in data.jobTypes) {
+    jobTypesList.push(data.jobTypes[key].name);
+  }
+
+  const changeStateSelection = (option) => {
+    setSelectedState(option.value);
+    const availableCities = data.states.find((c) => c.name === option.value);
+    setCitiesList(availableCities.cities);
+  };
+
+  const generateOptions = (data) => {
+    const tmp = [];
+
+    for (var key in data) {
+      tmp.push({
+        value: data[key],
+        label: data[key],
+      });
+    }
+    return tmp;
+  };
 
   return (
     <div id="container">
       <h2>Cascading or Dependent Dropdown using React</h2>
       <div>
         <label>Job Industry</label>
-        <select
-          placeholder="jobIndustry"
-          value={selectedJobIndustry}
-          onChange={(e) => setJobIndustry(e.target.value)}
-        >
-          <option>--Choose Job Industry--</option>
-          {data.jobIndustries.map((value, key) => {
-            return (
-              <option value={value.name} key={key}>
-                {value.name}
-              </option>
-            );
-          })}
-        </select>
+        <Select
+          placeholder={selectedJobIndustry}
+          defaultValue={selectedJobIndustry}
+          onChange={(e) => setJobIndustry(e.value)}
+          options={generateOptions(jobIndustriesList)}
+        />
       </div>
 
       <div>
         <label>State</label>
-        <select
-          placeholder="State"
-          value={selectedState}
-          onChange={(e) => setSelectedState(e.target.value)}
-        >
-          <option>--Choose State--</option>
-          {data.states.map((value, key) => {
-            return (
-              <option value={value.name} key={key}>
-                {value.name}
-              </option>
-            );
-          })}
-        </select>
+        <Select
+          placeholder={selectedState}
+          defaultValue={selectedState}
+          onChange={(e) => changeStateSelection(e)}
+          options={generateOptions(statesList)}
+        />
       </div>
 
       <div>
         <label>City</label>
-        <select
-          placeholder="City"
-          value={selectedCity}
-          onChange={(e) => setSelectedCity(e.target.value)}
-        >
-          <option>--Choose City--</option>
-          {availableCities?.cities.map((e, key) => {
-            return (
-              <option value={e.name} key={key}>
-                {e}
-              </option>
-            );
-          })}
-        </select>
+        <Select
+          placeholder={selectedCity}
+          defaultValue={selectedCity}
+          onChange={(e) => setSelectedCity(e.value)}
+          options={generateOptions(citiesList)}
+        />
       </div>
 
       <div>
         <label>Job Type</label>
-        <select
-          placeholder="jobType"
-          value={selectedJobType}
-          onChange={(e) => setJobType(e.target.value)}
-        >
-          <option>--Choose Job Type--</option>
-          {data.jobTypes.map((value, key) => {
-            return (
-              <option value={value.name} key={key}>
-                {value.name}
-              </option>
-            );
-          })}
-        </select>
+        <Select
+          placeholder={selectedJobType}
+          defaultValue={selectedJobType}
+          onChange={(e) => setJobType(e.value)}
+          options={generateOptions(jobTypesList)}
+        />
       </div>
     </div>
   );
