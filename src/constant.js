4b14d529c9242e70fa05183ddfd68a63fdb1a07a
Minh Nguyen <hongminh1620@gmail.com>
2022-04-16 18:35:26 -0700

    Put the constants to a file

---
+++ src/constant.js
@@ -0,0 +1,108 @@
+export const DATA_FOR_FILTERS = {
+    states: [
+      {
+        name: "CA",
+        cities: ["San Francisco", "Sunnyvale"],
+      },
+      {
+        name: "WA",
+        cities: ["Seattle"],
+      },
+    ],
+  
+    jobIndustries: [
+      {
+        name: "Management Occupations",
+        id: 11,
+      },
+      {
+        name: "Business and Financial Operations Occupations",
+        id: 13,
+      },
+      {
+        name: "Computer and Mathematical Occupations",
+        id: 15,
+      },
+      {
+        name: "Architecture and Engineering Occupations",
+        id: 17,
+      },
+      {
+        name: "Life, Physical, and Social Science Occupations",
+        id: 19,
+      },
+      {
+        name: "Community and Social Service Occupations",
+        id: 21,
+      },
+      {
+        name: "Legal Occupations",
+        id: 23,
+      },
+      {
+        name: "Educational Instruction and Library Occupations",
+        id: 25,
+      },
+      {
+        name: "Arts, Design, Entertainment, Sports, and Media Occupations",
+        id: 27,
+      },
+      {
+        name: "Healthcare Practitioners and Technical Occupations",
+        id: 29,
+      },
+      {
+        name: "Healthcare Support Occupations",
+        id: 31,
+      },
+      {
+        name: "Protective Service Occupations",
+        id: 33,
+      },
+      {
+        name: "Food Preparation and Serving Related Occupations",
+        id: 35,
+      },
+      {
+        name: "Building and Grounds Cleaning and Maintenance Occupations",
+        id: 37,
+      },
+      {
+        name: "Personal Care and Service Occupations",
+        id: 39,
+      },
+      {
+        name: "Sales and Related Occupations",
+        id: 41,
+      },
+      {
+        name: "Office and Administrative Support Occupations",
+        id: 43,
+      },
+      {
+        name: "Farming, Fishing, and Forestry Occupations",
+        id: 45,
+      },
+      {
+        name: "Construction and Extraction Occupations",
+        id: 47,
+      },
+      {
+        name: "Installation, Maintenance, and Repair Occupations",
+        id: 49,
+      },
+      {
+        name: "Production Occupations",
+        id: 51,
+      },
+      {
+        name: "Transportation and Material Moving Occupations",
+        id: 53,
+      },
+      {
+        name: "Military Specific Occupations",
+        id: 55,
+      },
+    ],
+  };
+  
