8e42776fb3373fc53c53bc595c32a93772df3ce8
Minh Nguyen <hongminh1620@gmail.com>
2022-04-17 15:17:23 -0700

    Add form

--- src/components/filters.js
+++ src/components/filters.js
@@ -1,11 +1,14 @@
 import React, { useState } from "react";
 import Select from "react-select";
+import { useForm } from "react-hook-form";
 
 import { DATA_FOR_FILTERS } from "./constant";
 
 export default function Filter() {
   const data = DATA_FOR_FILTERS;
-  const [selectedJobIndustry, setJobIndustry] = useState("--Choose Job Industry--");
+  const [selectedJobIndustry, setJobIndustry] = useState(
+    "--Choose Job Industry--"
+  );
   const [selectedState, setSelectedState] = useState("--Choose State--");
   const [selectedCity, setSelectedCity] = useState("--Choose City--");
   const [selectedJobType, setJobType] = useState("--Choose Job Type--");
@@ -43,48 +46,59 @@ export default function Filter() {
     return tmp;
   };
 
+  const {
+    filters,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
+  console.log(errors);
+
   return (
     <div id="container">
-      <h2>Cascading or Dependent Dropdown using React</h2>
-      <div>
-        <label>Job Industry</label>
-        <Select
-          placeholder={selectedJobIndustry}
-          defaultValue={selectedJobIndustry}
-          onChange={(e) => setJobIndustry(e.value)}
-          options={generateOptions(jobIndustriesList)}
-        />
-      </div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <h2>Cascading or Dependent Dropdown using React</h2>
+        <div>
+          <label>Job Industry</label>
+          <Select
+            placeholder={selectedJobIndustry}
+            defaultValue={selectedJobIndustry}
+            onChange={(e) => setJobIndustry(e.value)}
+            options={generateOptions(jobIndustriesList)}
+          />
+        </div>
 
-      <div>
-        <label>State</label>
-        <Select
-          placeholder={selectedState}
-          defaultValue={selectedState}
-          onChange={(e) => changeStateSelection(e)}
-          options={generateOptions(statesList)}
-        />
-      </div>
+        <div>
+          <label>State</label>
+          <Select
+            placeholder={selectedState}
+            defaultValue={selectedState}
+            onChange={(e) => changeStateSelection(e)}
+            options={generateOptions(statesList)}
+          />
+        </div>
 
-      <div>
-        <label>City</label>
-        <Select
-          placeholder={selectedCity}
-          defaultValue={selectedCity}
-          onChange={(e) => setSelectedCity(e.value)}
-          options={generateOptions(citiesList)}
-        />
-      </div>
+        <div>
+          <label>City</label>
+          <Select
+            placeholder={selectedCity}
+            defaultValue={selectedCity}
+            onChange={(e) => setSelectedCity(e.value)}
+            options={generateOptions(citiesList)}
+          />
+        </div>
 
-      <div>
-        <label>Job Type</label>
-        <Select
-          placeholder={selectedJobType}
-          defaultValue={selectedJobType}
-          onChange={(e) => setJobType(e.value)}
-          options={generateOptions(jobTypesList)}
-        />
-      </div>
+        <div>
+          <label>Job Type</label>
+          <Select
+            placeholder={selectedJobType}
+            defaultValue={selectedJobType}
+            onChange={(e) => setJobType(e.value)}
+            options={generateOptions(jobTypesList)}
+          />
+        </div>
+        <button type="submit">Find Companies</button>
+      </form>
     </div>
   );
 }
