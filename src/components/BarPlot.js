b28a51b81724f646b135016a33fb6c0e24eb6c49
Minh Nguyen <hongminh1620@gmail.com>
2022-04-16 22:06:21 -0700

    Update the components to abstract the code

--- src/components/BarPlot.js
+++ src/components/BarPlot.js
@@ -1,12 +1,44 @@
 import React, { memo } from "react";
-import Chart from 'react-apexcharts'
+import Chart from "react-apexcharts";
 
-const BarPlot = (props) =>{
-  return(
+const BarPlot = (props) => {
+  const options = {
+    chart: {
+      type: "bar",
+      height: 350,
+    },
+    plotOptions: {
+      bar: {
+        borderRadius: 4,
+        horizontal: true,
+      },
+    },
+    dataLabels: {
+      enabled: false,
+    },
+    xaxis: {
+      categories: props.options,
+    },
+    title: {
+      text: props.title,
+      align: "center",
+      margin: 10,
+      offsetX: 0,
+      offsetY: 0,
+      floating: false,
+      style: {
+        fontSize: "14px",
+        fontWeight: "bold",
+        fontFamily: undefined,
+        color: "#263238",
+      },
+    },
+  };
+  return (
     <div>
       <Chart
-        options={props.options}
-        series={[{data: props.data}]}
+        options={options}
+        series={[{ data: props.data }]}
         type="bar"
         height={350}
         title={props.title}
--- src/components/linechart.js
+++ src/components/linechart.js
@@ -1,11 +1,52 @@
-import Chart from 'react-apexcharts'
-import React,  { memo } from "react";
+import Chart from "react-apexcharts";
+import React, { memo } from "react";
 
-const LineChart = (props) =>{
-    return(
-        <div>
-            <Chart options={props.options} series={props.series} type="line" height={350} />
-        </div>
-    );
+const LineChart = (props) => {
+  const options = {
+    chart: {
+      height: 350,
+      type: "line",
+      zoom: {
+        enabled: false,
+      },
+    },
+    dataLabels: {
+      enabled: false,
+    },
+    stroke: {
+      curve: "straight",
+    },
+    title: {
+      text: props.title,
+      align: "center",
+    },
+    grid: {
+      row: {
+        colors: ["#f3f3f3", "transparent"], // takes an array which will be repeated on columns
+        opacity: 0.5,
+      },
+    },
+    xaxis: {
+      categories: props.options,
+    },
+  };
+
+  const series = [
+    {
+      name: "Approval Rate",
+      data: props.series,
+    },
+  ]
+  return (
+    <div>
+      <Chart
+        options={options}
+        series={series}
+        title={props.title}
+        type="line"
+        height={350}
+      />
+    </div>
+  );
 };
 export default memo(LineChart);
