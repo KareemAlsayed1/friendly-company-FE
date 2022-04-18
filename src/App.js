aa5535bff2ab5c9f2c62606863c0e15a7f7d11a0
Minh Nguyen <hongminh1620@gmail.com>
2022-04-17 20:07:20 -0700

    Add new route to the result

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import React from "react";
 
 import CompanyStats from "./components/CompanyStats";
 import Home from "./components/Home";
+import FilterResults from "./components/FilterResults";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Home/>} />
         <Route exact path="/company" element={<CompanyStats/>} />
+        <Route exact path="/results" element={<FilterResults/>} />
         <Route path="*" element={<Navigate to ="/" />}/>
       </Routes>
     </Router>
