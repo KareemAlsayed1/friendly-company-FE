e91b0c5ed7da1bf9d6a0f64f6d1af27f4dff64de
Minh Nguyen <hongminh1620@gmail.com>
2022-04-17 18:38:44 -0700

    Add Search Bar to the home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Filter from "./filters";
+import SearchBar from "./SearchBar";
 
 export default function Home() {
     return (
         <div>
             <Filter />
+            <SearchBar />
         </div>
     )
 }
